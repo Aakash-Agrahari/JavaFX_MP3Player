dbc90bc546d6f3624d3b547a6d79d55b54ba4548
Aakash <aakashagrahari45@gmail.com>
2023-09-08 22:49:25 +0530

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module com.example.demoprog21 {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires javafx.media;
+
+    opens com.example.demoprog21 to javafx.fxml;
+    exports com.example.demoprog21;
+}
